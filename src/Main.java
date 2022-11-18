f433c92218b6461eb5392106fab8dfa7fc59917c
LuizG <luizguilherme3m@hotmail.com>
2022-11-18 14:22:49 -0300

    Projeto5

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import dio.gof.SingletonEager;
+import dio.gof.SingletonLazy;
+import dio.gof.SingletonLazyHolder;
+
+public class Main {
+    public static void main(String[] args) {
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+        lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+    }
+}
